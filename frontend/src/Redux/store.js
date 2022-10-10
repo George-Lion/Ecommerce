d2d6f5281253ff9f6604a6410c6efcf784bad445
Jorge Leon <jorgeleonpicture@gmail.com>
2022-10-10 12:17:07 +0200

    login and logout

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -3,21 +3,33 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productListReducer, productDetailsReducer } from "./Reducers/ProductReducers.js";
 import { cartReducer } from "./Reducers/CartReducers";
+import { userLoginReducer } from "./Reducers/userReducers.js";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
-  : []
+  : [];
+
+// login
+
+const userInfoFromLocalStorange = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 
 const initialState = {
   cart: {
     cartItems: cartItemsFromLocalStorage,
-  }
+  },
+  userLogin: {
+    userInfo: userInfoFromLocalStorange
+  },
 };
 const middleware = [thunk];
 
